54fbb7433db20edafb7f89d4b75a2bd226337863
Nishank Singla <nishank.singla@sjsu.edu>
2016-11-15 17:06:39 -0800

    index page
    Added header, footer, signup and login modal, property search

--- RabbitMQ-Client/angularApp/app.js
+++ RabbitMQ-Client/angularApp/app.js
@@ -64,114 +64,36 @@ app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'Notifi
 
 }]);
 
-app.controller('indexController', ['$scope', '$http', 'ngProgress', '$state', '$rootScope', function ($scope, $http, ngProgress, $state, $rootScope) {
+app.controller('indexController', ['$scope', '$http', 'ngProgress', '$state', '$rootScope', '$uibModal', function ($scope, $http, ngProgress, $state, $rootScope, $uibModal) {
 
-    // $scope.siginPage = false;
-    // $scope.loginStatus = false;
-    // $scope.numCartItems = 0;
+    $scope.openSignup = function () {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'signupmodal.html',
+            controller: 'signupController',
+            windowClass:"signupmodal",
+            resolve: {
 
-    // $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-    //     if (!$scope.loginStatus) {
-    //         $http.get('/login/status').
-    //             success(function (data, status, headers, config) {
-    //                 if (data.user) {
-    //                     $scope.loginStatus = true;
-    //                     $scope.firstname = data.user.firstname;
-    //                     if (fromState === "Cart") {
-    //                         $state.go('Cart');
-    //                     }
-    //                     if (toState.name === "SignIn" || toState.name === "Register") {
-    //                         $state.go('/');
-    //                     }
-    //                 } else {
-    //                     if (toState.name !== "Register") {
-    //                         $state.go('SignIn');
-    //                     }
+            }
+        });
+    };
 
-    //                 }
-    //                 // else if (toState.name === "MyAccount" || toState.name === "Sell" || toState.name === "checkout") {
-    //                 //     $state.go('/');
-    //                 // }
-    //             }).
-    //             error(function (data, status, headers, config) {
+    $scope.openLogin = function () {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'loginmodal.html',
+            controller: 'loginController',
+            windowClass:"loginmodal",
+            resolve: {
 
-    //             });
-    //     } else if ($scope.loginStatus) {
-    //         if (toState.name === "SignIn" || toState.name === "Register") {
-    //             $state.go('/');
-    //         }
-    //     }
-    //     if (toState.name === "SignIn" || toState.name === "Register" || toState.name === "checkout") {
-    //         $scope.siginPage = true;
-    //     } else {
-    //         $scope.siginPage = false;
-    //     }
-    // });
-
-    // $scope.logout = function () {
-    //     $http.get('/login/logout').
-    //         success(function (data, status, headers, config) {
-    //             if (data.status) {
-    //                 $scope.loginStatus = false;
-    //                 $state.go('/');
-    //             }
-    //         }).
-    //         error(function (data, status, headers, config) {
-
-    //         });
-    // }
-
-    // $scope.getCart = function () {
-    //     $http.get("/ebayCart")
-    //         .success(function (data) {
-    //             if (data.status !== "noitem") {
-    //                 $scope.numCartItems = data.length;
-    //             }else{
-    //                 $scope.numCartItems = 0;
-    //             }
-    //         })
-    //         .error(function (err) {
-
-    //         })
-    // }
-
-    // $scope.categories = {
-    //     0: "All Categories",
-    //     1: "Antiques",
-    //     2: "Art",
-    //     3: "Baby",
-    //     4: "Books",
-    //     5: "Business & Industrial",
-    //     6: "Cameras & Photo",
-    //     7: "Cell Phones & Accessories",
-    //     8: "Clothing, Shoes & Accessories",
-    //     9: "Coins & Paper Money",
-    //     10: "Collectibles",
-    //     11: "Computers/Tablets & Networking",
-    //     12: "Consumer Electronics",
-    //     13: "Crafts",
-    //     14: "Dolls & Bears",
-    //     15: "DVDs & Movies",
-    //     16: "eBay Motors",
-    //     17: "Entertainment Memorabilia",
-    //     18: "Gift Cards & Coupons",
-    //     19: "Health & Beauty",
-    //     20: "Home & Garden",
-    //     21: "Jewelry & Watches",
-    //     22: "Music",
-    //     23: "Musical Instruments & Gear",
-    //     24: "Pet Supplies",
-    //     25: "Pottery & Glass",
-    //     26: "Real Estate",
-    //     27: "Specialty Services",
-    //     28: "Sporting Goods",
-    //     29: "Sports Mem, Cards & Fan Shop",
-    //     30: "Stamps",
-    //     31: "Tickets & Experiences",
-    //     32: "Toys & Hobbies",
-    //     33: "Travel",
-    //     34: "Video Games & Consoles",
-    //     35: "Everything Else"
-    // }
-
+            }
+        });
+    };
 }]);
+
+app.controller('signupController', function ($scope, $uibModalInstance) {
+
+});
+app.controller('loginController', function ($scope, $uibModalInstance) {
+
+});
