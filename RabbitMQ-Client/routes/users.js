584668aa96afa7dd9668c2bd748a1e5da0ac0210
Venkatesh Ramanujam Rangarajan <venkateshramanujam.rangarajan@sjsu.edu>
2016-11-16 18:49:55 -0800

    registration module

---
+++ RabbitMQ-Client/routes/users.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mq_client = require('../rpc/client');
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/register', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
+  var msg_payload  = {firstname: req.body.firstname, lastname:req.body.lastname,email:req.body.email,password:req.body.password,logintime:new Date().toDateString()};
+  mq_client.make_request('register_queue',msg_payload, function(err,results){
+    if(err){
+      return done(err);
+    }
+    else
+    {
+      if(results.code == 200){
+        console.log('response from server');
+        //to do redirect to some page
+        res.send("Resistration success");
+      }
+      else {
+        console.log("Invalid signup... record duplication");
+        res.send("Resistration failure");
+        return done(null, false);
+      }
+    }
+  });
+});
+module.exports = router;
